adf8e3a2ca835d2113d430a14e4145c6380f8445
lizb <loo4love90@gmail.com>
2026-05-28 17:14:58 +0900

    0525-update

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from app.models import mongodb
 from app.models.book import BookModel
+from app.book_scraper import NaverBookScraper
 
 
 app = FastAPI()
@@ -19,14 +20,14 @@ templates = Jinja2Templates(directory=BASE_DIR/ "templates")
 
 @app.get("/",response_class=HTMLResponse)
 async def root(request: Request):
-    book=BookModel(
-        keyword="python",
-        publisher="lizb",
-        price=1200,
-        image="me.png"
-    )
-    save_book= await mongodb.engine.save(book)
-    print(save_book.model_dump(),flush=True)
+    # book=BookModel(
+    #     keyword="python",
+    #     publisher="lizb",
+    #     price=1200,
+    #     image="me.png"
+    # )
+    # save_book= await mongodb.engine.save(book)
+    # print(save_book.model_dump(),flush=True)
     return templates.TemplateResponse(
         request,
         "index.html",
@@ -36,10 +37,36 @@ async def root(request: Request):
 
 @app.get("/search",response_class=HTMLResponse)
 async def read_item(request:Request,q:str):
+    keyword=q
+
+    if not keyword:
+        return templates.TemplateResponse(
+            request=request,
+            name="index.html",
+            context={"message":"검색어를 입력해주세요"}
+        )
+    naver_book_scraper=NaverBookScraper()
+
+    books = await naver_book_scraper.search(keyword,10)
+
+    book_models=[]
+
+    for book in books:
+        print(book)
+        book_model=BookModel(
+            keyword=keyword,
+            publisher=book["publisher"],
+            price=int(book.get("discount") or 0),
+            image=book["image"]
+        )
+        book_models.append(book_model)
+
+    await mongodb.engine.save_all(book_models)   
+
     return templates.TemplateResponse(
-        request,
-        "index.html",
-        {"keyword":q}
+        request=request,
+        name="index.html",
+        context= {"keyword":q,"books":book_models}
     )
 
 
